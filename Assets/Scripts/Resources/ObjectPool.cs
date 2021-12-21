0f031c231777bb0996575fd87af5e0081506b19d
Mete Yıldırım <43517475+meteyldrm@users.noreply.github.com>
2021-12-21 20:21:57 +0300

    Init

---
+++ Assets/Scripts/Resources/ObjectPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Resources {
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private int poolGrowSize;
+
+        private readonly Stack<GameObject> _availableObjects = new Stack<GameObject>();
+
+        private void Awake()
+        {
+            GrowPool();
+        }
+
+        private void GrowPool()
+        {
+            for (var i = 0; i < poolGrowSize; i++)
+            {
+                var instanceToAdd = Instantiate(prefab, transform, true);
+                AddToPool(instanceToAdd);
+            }
+        }
+
+        public void AddToPool(GameObject instance)
+        {
+            _availableObjects.Push(instance);
+            instance.SetActive(false);
+        }
+
+        public GameObject GetFromPool()
+        {
+            if(_availableObjects.Count == 0)
+                GrowPool();
+
+
+            var inst = _availableObjects.Pop();
+            inst.SetActive(true);
+            return inst;
+        }
+    }
+}
